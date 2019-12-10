608970c7641e2c7cc88b0d708726e62dce7f16f3
ismailSD <www.vvcs-fat@hotmail.co.uk>
2019-12-10 23:37:24 +0000

    Update test.js

--- test/test.js
+++ test/test.js
@@ -6,11 +6,8 @@ var server = require('../server.js');
 describe('node hello world console app', function() {
   describe('#indexOf()', function() {
     it("returns status 200", function(done) {
-        
-      request(server.url, function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
+      expect(server.status).to.equal(200);
+      done();
     });
   });
 });
