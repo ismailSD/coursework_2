343bb23dbf46b9cf3f8a95251d4ad9336d64201c
ismailSD <www.vvcs-fat@hotmail.co.uk>
2019-12-10 23:12:04 +0000

    update test.js file

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 var assert = require('assert');
+var server = require('../server.js');
+
 describe('node hello world console app', function() {
   describe('#indexOf()', function() {
     it('it should print status code of 200', function() {
-      assert.equal(200);
+        var code = server.status;
+      assert.equal(code).to.equal(200);
     });
   });
 });
