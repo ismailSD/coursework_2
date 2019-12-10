08ee9ab3b9b829ddbb8fd954cfa2723cd7b7f5f3
ismailSD <www.vvcs-fat@hotmail.co.uk>
2019-12-10 23:28:51 +0000

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
 var assert = require('assert');
+var expect    = require("chai").expect;
 var server = require('../server.js');
-
 describe('node hello world console app', function() {
   describe('#indexOf()', function() {
-    it('it should print status code of 200', function() {
-        var code = server.status;
-      assert.equal(code).to.equal(200);
+    it("returns status 200", function(done) {
+        
+      request(server.url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
   });
 });
