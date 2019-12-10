b58b5f0c4540bbb73d1b39ed947d59ced359ab1f
Ubuntu <iadam200@LabVM.wrnsw53famdetgc3u5bupofbtg.bx.internal.cloudapp.net>
2019-12-10 22:52:44 +0000

    add test with mocha

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('node hello world console app', function() {
+  describe('#indexOf()', function() {
+    it('It should log hello world with port and hostname', function() {
+      assert.equal("Hello world");
+    });
+  });
+});
