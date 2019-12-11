06ef6b7ec29b4f99df17b869fbabbb354394eb18
Ubuntu <iadam200@LabVM.wrnsw53famdetgc3u5bupofbtg.bx.internal.cloudapp.net>
2019-12-11 00:53:51 +0000

    modified server.js

--- server.js
+++ server.js
@@ -4,20 +4,12 @@ var podname= process.env.HOSTNAME;
 var PORT = process.env.PORT;
 var startTime;
 var host;
-var status;
 
 var handleRequest = function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
   response.write("Hello World! | Running on: ");
   response.write(host);
-  
-  
-   console.log("status code" +response.statusCode);
-
-  
-  status = response.statusCode;
-  
   response.end(" | v=1\n");
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
 }
