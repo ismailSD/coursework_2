aef2cf35082edad255fec3c0748912246dc9b6e1
ismailSD <www.vvcs-fat@hotmail.co.uk>
2019-12-11 00:07:45 +0000

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 echo 'The following command terminates the "npm start" process using its PID'
 set -x
-kill $(cat /var/run/myProcess.pid)
+trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
